d566371731e338379a68d9b7ee92099a68bc97ae
ajugjacob <ajugjacob@gmail.com>
2022-07-26 12:47:48 +0530

    Added requirements

--- setup.py
+++ setup.py
@@ -11,6 +11,20 @@ setup(
     scripts=['start.sh'],
 
     packages=find_packages(),
+    
+    install_requires=[
+	'cachelib==0.9.0',
+	'click==8.1.3',
+	'Flask==2.1.3',
+	'Flask-Login==0.6.1',
+	'Flask-Session==0.4.0',
+	'Flask-SQLAlchemy==2.5.1',
+	'itsdangerous==2.1.2',
+	'Jinja2==3.1.2',
+	'MarkupSafe==2.1.1',
+	'SQLAlchemy==1.4.39',
+	'Werkzeug==2.0.0'
+    ],
 
     include_package_data = True,
 
